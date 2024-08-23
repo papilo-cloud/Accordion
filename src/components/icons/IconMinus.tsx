cc62c6d451165413db9a29334de944c06c19ef02
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-08-23 23:39:30 +0100

    fixed icon ref problem

--- src/components/icons/IconMinus.tsx
+++ src/components/icons/IconMinus.tsx
@@ -1,14 +1,16 @@
-import type {SVGProps } from "react"
-import { Ref } from "react"
+import type {Ref, SVGProps } from "react"
 
-const IconMinus = (props:SVGProps<SVGSVGElement>, ref:Ref<SVGSVGElement>) => {
+const IconMinus = (props:SVGProps<SVGPathElement>, ref:Ref<SVGSVGElement>) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" 
         fill="none" 
         viewBox="0 0 30 31"
-        ref={ref} {...props}>
+        // ref={ref}
+        >
             <path fill="#301534" 
-                d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"/>
+                d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"
+                {...props}
+              />
     </svg>
   )
 }
