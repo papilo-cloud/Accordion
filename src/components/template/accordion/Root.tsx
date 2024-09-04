d7e9e1d7ef468fb18dd69896a5a0e43e6e176946
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-04 23:44:33 +0100

    added options between displaying single or multiple panels

--- src/components/template/accordion/Root.tsx
+++ src/components/template/accordion/Root.tsx
@@ -1,11 +1,13 @@
 import clsx from 'clsx'
-import { ComponentProps, ReactNode } from 'react'
+import { ComponentProps, createContext, ReactNode } from 'react'
 
+export const RootContext = createContext('single')
 interface RootProps extends ComponentProps<'div'> {
-    children: ReactNode
+    children: ReactNode;
+    type: 'single'| 'multiple'
 }
 
-const Root = ({children, className, ...props}: RootProps) => {
+const Root = ({children, className, type, ...props}: RootProps) => {
   return (
     <div className={clsx(' bg-white max-[550px]:w-full w-[500px] p-5 rounded-xl flex flex-col gap-5 shadow-md', className) }{...props}>
         {children}
